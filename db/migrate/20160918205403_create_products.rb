9c3e48b6c01fa60f73c9a40e2a937e8a57e43f9d
Sangun Chae <josuchae@gmail.com>
2016-09-18 23:00:23 +0200

    4.1 added product scaffolding

---
+++ db/migrate/20160918205403_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
