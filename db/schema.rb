8a000a0833d5ac60552ef0c899ce46d2d5f228ce
Sangun Chae <josuchae@gmail.com>
2016-09-22 14:54:36 +0200

    4.2 added color field and image tag (basic product page styling) to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918205403) do
+ActiveRecord::Schema.define(version: 20160922114240) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160918205403) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end
