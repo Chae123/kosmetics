3ef359654b88b04731192ec434bdaf5127e2a912
Sangun Chae <josuchae@gmail.com>
2016-09-30 12:06:50 +0200

    5.1 created dynamic links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  
   get 'static_pages/about'
 
   get 'static_pages/contact'
+
+  get 'static_pages/brands'
+
+  get 'static_pages/index'
 
   # root 'static_pages#index'
 
