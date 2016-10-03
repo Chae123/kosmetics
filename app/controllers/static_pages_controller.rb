6c531822d938add4cf2c23ed6e2ff8b050be5422
Sangun Chae <josuchae@gmail.com>
2016-10-03 12:27:00 +0200

    5.3 incl. contact form w/ ActionMailer method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,3 +9,15 @@ class StaticPagesController < ApplicationController
 		@products = Product.limit(3)
 	end
 end
+
+class StaticPagesController < ApplicationController
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		ActionMailer::Base.mail(:from => @email,
+			:to => "xcalibur23@hotmail.com",
+			:subject => "A new contact form message from #{@name}",
+			:body => @message).deliver_now
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :orders, only: [:index, :show, :create, :destroy]
-  
-  get 'static_pages/about'
+	resources :products
+	resources :orders, only: [:index, :show, :create, :destroy]
+	
+	get 'static_pages/about'
 
-  get 'static_pages/contact'
+	get 'static_pages/contact'
 
-  get 'static_pages/brands'
+	get 'static_pages/brands'
 
-  get 'static_pages/index'
+	get 'static_pages/index'
 
-  # root 'static_pages#index'
+	get 'static_pages/contact'
 
-  root 'static_pages#landing_page'
+	get 'static_pages/thank_you'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	post 'static_pages/thank_you'
+
+	# root 'static_pages#index'
+
+	root 'static_pages#landing_page'
+
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
