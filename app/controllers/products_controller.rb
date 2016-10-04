8070dd9280c87ed7bac2f384d75b0668dd7c217c
Sangun Chae <josuchae@gmail.com>
2016-10-04 22:22:24 +0200

    5.5 install CanCanCan auth. gem and define abilities

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,8 @@ class ProductsController < ApplicationController
 		@products = Product.all
 		if params[:q]
 			search_term = params[:q]
-			@products = Product.where("name LIKE ?", "%#{search_term}%")
+			@products = Product.where("name LIKE ?", "%#{search_term}%") if Rails.env.development?
+			@products = Product.where("name ilike ?". "%#{search_term}") if Rails.env.production?
 		# return our filtered list here
 		else
 			@products = Product.all
