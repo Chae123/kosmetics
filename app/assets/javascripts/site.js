2148666d8ae78e505905cac0d9a977be6dd02dfc
Sangun Chae <josuchae@gmail.com>
2016-10-07 14:16:43 +0200

    5.8b javascript fix

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -4,19 +4,19 @@
 
 
 $(document).on('turbolinks:load', function(){
- $('.rating').raty( {
-     scoreName: 'comment[rating]',
-   starHalf        : <% image_path('star-half.png') %>,
-   starOff         : <% image_path('star-off.png') %>,
-   starOn          : <% image_path('star-on.png') %>
- });
- $('.rated').raty({ 
-        readOnly: true,
-   starHalf        : <% image_path('star-half.png') %>,
-   starOff         : <% image_path('star-off.png') %>,
-   starOn          : <% image_path('star-on.png') %>,
-        score: function() {
-            return $(this).attr('data-score');
-        }
-    });
+$('.rating').raty( {
+    scoreName: 'comment[rating]',
+  starHalf        : '<%= image_path('star-half.png') %>',
+  starOff         : '<%= image_path('star-off.png') %>',
+  starOn          : '<%= image_path('star-on.png') %>'
+});
+$('.rated').raty({ 
+    readOnly: true,
+starHalf        : '<%= image_path('star-half.png') %>',
+starOff         : '<%= image_path('star-off.png') %>',
+starOn          : '<%= image_path('star-on.png') %>',
+       score: function() {
+           return $(this).attr('data-score');
+       }
+   });
 });
