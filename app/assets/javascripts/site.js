5de68964736b2ac689fdf2952460d24e2ebb88a2
Sangun Chae <josuchae@gmail.com>
2016-10-07 13:04:21 +0200

    5.8b javascript fix

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,7 @@
 // Alert messages
-('.alert').delay(2000).fadeOut();
+// ('.alert').delay(2000).fadeOut();
+$(".alert-message").alert();
+window.setTimeout(function() { $(".alert-message").alert('close'); }, 2000)
 
 // Rating Stars
 $(document).on('turbolinks:load', function(){
