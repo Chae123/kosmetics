1e4555633365475765d6fac91830eef78209ec1d
Sangun Chae <josuchae@gmail.com>
2016-10-07 14:05:53 +0200

    5.8b javascript fix

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,21 +2,21 @@
 
 // Rating Stars
 
+
 $(document).on('turbolinks:load', function(){
  $('.rating').raty( {
      scoreName: 'comment[rating]',
-   starHalf        : <% asset_path('star-half.png') %>,
-   starOff         : <% asset_path('star-off.png') %>,
-   starOn          : <% asset_path('star-on.png') %>
+   starHalf        : <% image_path('star-half.png') %>,
+   starOff         : <% image_path('star-off.png') %>,
+   starOn          : <% image_path('star-on.png') %>
  });
  $('.rated').raty({ 
         readOnly: true,
-   starHalf        : <% asset_path('star-half.png') %>,
-   starOff         : <% asset_path('star-off.png') %>,
-   starOn          : <% asset_path('star-on.png') %>,
+   starHalf        : <% image_path('star-half.png') %>,
+   starOff         : <% image_path('star-off.png') %>,
+   starOn          : <% image_path('star-on.png') %>,
         score: function() {
             return $(this).attr('data-score');
         }
     });
 });
-
