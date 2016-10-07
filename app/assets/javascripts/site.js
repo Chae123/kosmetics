3c0cc5af6e5e22cca8a7f95b93682bd00ae481bc
Sangun Chae <josuchae@gmail.com>
2016-10-07 13:53:36 +0200

    5.8b javascript fix

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,22 @@
 // Alert messages
 
 // Rating Stars
+
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-	readOnly: true,
-	score: function() {
-		return $(this).attr('data-score');
-	}
-	});
+ $('.rating').raty( {
+     scoreName: 'comment[rating]',
+   starHalf        : <% asset_path('star-half.png') %>,
+   starOff         : <% asset_path('star-off.png') %>,
+   starOn          : <% asset_path('star-on.png') %>
+ });
+ $('.rated').raty({ 
+        readOnly: true,
+   starHalf        : <% asset_path('star-half.png') %>,
+   starOff         : <% asset_path('star-off.png') %>,
+   starOn          : <% asset_path('star-on.png') %>,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
 });
 
