2858dc67645e9320f9f66149fc47af723bdc2f7f
Sangun Chae <josuchae@gmail.com>
2016-10-09 18:11:25 +0200

    5.10 include admin flag

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,11 @@ class Ability
 
 	def initialize(user)
 		user ||= User.new #guest user (not logged in)
-		can :manage, User, id: user.id
+		if user.admin?
+			can :manage, :all
+		else
+			can :read, :all
+		end
 		# Define abilities for the passed in user here. For example:
 		#
 		#   user ||= User.new # guest user (not logged in)
@@ -30,5 +34,9 @@ class Ability
 		#
 		# See the wiki for details:
 		# https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
+		# Before:
+		# user ||= User.new #guest user (not logged in)
+		# can :manage, User, id: user.id
 	end
 end
