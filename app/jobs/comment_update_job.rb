954c148f1af0575f1868b1d6d833c7902378d637
Sangun Chae <josuchae@gmail.com>
2016-10-27 13:08:38 +0200

    6.14 fix typo

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -1,14 +1,13 @@
 class CommentUpdateJob < ApplicationJob
-  queue_as :default
+	queue_as :default
 
-  def perform(comment, current_user)
-    # Do something later
-    				ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
-  end
+	def perform(comment, current_user)
+		ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
+	end
 
-  priveate
+	private
 
-  	def render_comment(comment, current_user)
-  		CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })
-  	end
+		def render_comment(comment, current_user)
+			CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })
+		end
 end
