8f4036d2d73014cb0cae110d630d470d8bf743a8
Sangun Chae <josuchae@gmail.com>
2016-10-27 13:57:58 +0200

    6.14 debugging

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -9,6 +9,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
 	received: function(data) {
 	// Called when there's incoming data on the websocket for this channel
+	console.log(data);
 	$(".alert.alert-info").show();
 	$('.product-reviews').prepend(data.comment);
 	refreshRating();
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,13 +3,13 @@ class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :product
 
+	scope :rating_desc, -> { order(rating: :desc) }
+	scope :rating_asc, -> { order(rating: :asc) }
+
 	validates :body, presence: true
 	validates :user, presence: true
 	validates :product, presence: true
 	validates :rating, numericality: { only_integer: true }
-
-	scope :rating_desc, -> { order(rating: :desc) }
-	scope :rating_asc, -> { order(rating: :asc) }
 
 	after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 end
