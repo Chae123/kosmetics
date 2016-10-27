3905aa93fc8d160a3d417286a3ecfbd35959362c
Sangun Chae <josuchae@gmail.com>
2016-10-27 12:54:26 +0200

    6.14 include ActiveJob and update strip for production testing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@ class CommentsController < ApplicationController
 		@product = Product.find(params[:product_id])
 		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
+		@user = current_user
 		respond_to do |format|
 			if @comment.save
 				# ActionCable.server.broadcast 'product_channel', comment: @comment
-				ProductChannel.broadcast_to @product.id, comment: @comment
 				format.html {redirect_to @product, notice: 'Review was created successfully.'}
 				format.json { render :show, status: :created, location: @product }
 				format.js
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,20 @@
-if Rails.env.production?
-	Rails.configuration.stripe = {
-		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-		:secret_key => ENV['STRIPE_SECRET_KEY']
-	}
-else
-	Rails.configuration.stripe = {
-		:publishable_key => 'pk_test_gmEWl2dMfdghxjawecYqJb4O',
-		:secret_key => 'sk_test_n2lAAlPL4aaoP5JFbNedSniJ'
-	}
+# if Rails.env.production?
+# 	Rails.configuration.stripe = {
+# 		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+# 		:secret_key => ENV['STRIPE_SECRET_KEY']
+# 	}
+# else
+# 	Rails.configuration.stripe = {
+# 		:publishable_key => 'pk_test_gmEWl2dMfdghxjawecYqJb4O',
+# 		:secret_key => 'sk_test_n2lAAlPL4aaoP5JFbNedSniJ'
+# 	}
+# end
+
+#revert below code after submission approval with above code:
+Rails.configuration.stripe = {
+       :publishable_key => 'pk_test_gmEWl2dMfdghxjawecYqJb4O',
+       :secret_key => 'sk_test_n2lAAlPL4aaoP5JFbNedSniJ'
+   }
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
